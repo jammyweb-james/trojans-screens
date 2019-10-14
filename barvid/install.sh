cb4891edcf7f39b024b02fbfd2ee38bbea573171
jammyweb-james <james.work@jammyweb.org.uk>
2019-10-14 23:28:44 +0100

    First checkin of install.sh

---
+++ barvid/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Error out if anything fails.
+set -e
+
+# Make sure script is run as root.
+if [ "$(id -u)" != "0" ]; then
+  echo "Must be run as root with sudo! Try: sudo ./install.sh"
+  exit 1
+fi
+
+
+echo "Copy .ini file to /boot"
+cp video_looper.ini /boot
