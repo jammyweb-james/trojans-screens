3a6c063a9eb76c63323fcc206bea41ccd8cf60d1
jammyweb-james <james.work@jammyweb.org.uk>
2020-02-15 09:55:22 +0000

    Create install.sh

---
+++ splash/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Error out if anything fails.
+set -e
+
+# Make sure script is run as root.
+if [ "$(id -u)" != "0" ]; then
+  echo "Must be run as root with sudo! Try: sudo ./install.sh"
+  exit 1
+fi
+
+
+echo "Update html files"
+cp *.php /var/www/html
+
+echo "update images"
+cp *.png /var/www/html/assets/images
+cp *.jpg /var/www/html/assets/images
